c219983a20aeabf2e0ba5e9282d9ebe57294cbc5
nidhiraghute <85737394+nidhiraghute@users.noreply.github.com>
2022-02-04 09:47:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ rightWrist_x = 0;
 rightWrist_y = 0;
 leftWrist_x = 0;
 leftWrist_y = 0;
+scoreleftWrist = 0;
+song_name = "";
 
 function setup(){
     canvas = createCanvas(600,530);
@@ -23,6 +25,24 @@ function preload(){
 
 function draw(){
     image(video,0,0,600,530);
+
+    fill("#00ff00");
+    stroke("#ff0000");
+
+    song_name = Peter_pan_song.isPlaying();
+    console.log(song_name);
+
+    if(scoreleftWrist > 0.2){
+        circle(leftWrist_x,leftWrist_y,20);
+        Harry_potter_theme_song.stop();
+        if(song_name == false){
+            Peter_pan_song.play();
+        }
+        else{
+            console.log("Song Name: Peter Pan Song");
+            document.getElementById("song_id").innerHTML = "Song Name: Peter Pan Song";
+        }
+    }
 }
 
 function modelLoaded(){
@@ -32,7 +52,10 @@ function modelLoaded(){
 function gotposes(results){
     if(results.length > 0){
         console.log(results);
-         
+
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        console.log(scoreleftWrist);
+
         leftWrist_x = results[0].pose.leftWrist.x;
         leftWrist_y = results[0].pose.leftWrist.y;
         console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
